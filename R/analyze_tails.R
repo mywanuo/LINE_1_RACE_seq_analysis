d59a19e43cc7da2cbfd8e4d23758053b428814fd
smaegol <p.krawczyk@ibb.waw.pl>
2017-10-30 15:23:35 +0100

    R analysis of output - producing figures

--- R/analyze_tails.R
+++ R/analyze_tails.R
@@ -24,24 +24,26 @@ library(ggplot2)
 library(plyr)
 library(dplyr)
 library(ggseqlogo)
-#library(ggseqlogo)
 
+melt_data_localization <- "/home/smaegol/storage/analyses/tail_seq_3/new/LINE_1_RACE_seq_analysis/flowcells_all/processing_out/test_fin.tsv"
 
-melt_data_localization <- "/home/smaegol/storage/analyses/tail_seq_3/new/LINE_1_RACE_seq_analysis/flowcell2/fastq/processing_out_sabre/test_nowy.tsv"
 
 # read tailing information to data.frame using data.table
 tails_data_melt <- fread(melt_data_localization, sep = "\t", header = T, stringsAsFactors = T,
     data.table = F, showProgress = TRUE)  # read data
 
+
+#tails_data_melt[tails_data_melt$localization=='JADRO',]$localization<-"TOTAL"
 tails_data_melt$tailed <- tails_data_melt$tail_length > 0  #mark tailed reads
 tails_data_melt$mapped <- tails_data_melt$mapping_position != -1  #mark mapped reads
 tails_data_mapped <- tails_data_melt[tails_data_melt$mapped != 0, ]  #discard unmapped reads
 
+#head(tails_data_mapped$transcript)
 
 # fix of flase_no_tail - should be assigned no tail cause CTGAC was found in the
 # clipped fragment - to be fixed in python script
 tails_data_mapped$tail_type <- as.character(tails_data_mapped$tail_type)
-tails_data_mapped[tails_data_mapped$tail_type == "false_no_tail_no_CTGAC", ]$tail_type = "no_tail"
+tails_data_mapped[tails_data_mapped$tail_type == "false_no_tail_no_CTGAC", ]$tail_type <- "no_tail"
 
 tails_data_mapped$ref_name_R5 <- as.character(tails_data_mapped$ref_name_R5)
 tails_data_mapped$ref_name_R3 <- as.character(tails_data_mapped$ref_name_R3)
@@ -57,12 +59,13 @@ tails_data_mapped$terminal_nucleotides<-gsub("T","U",tails_data_mapped$terminal_
 
 # in further analyses use only those read which got CTGAC delimiter identified in
 # the clipped fragment
+# for reporter analyses all mapped reads got CTGAC_R5 variable = 1 (because of short reads) so they will be included in the analysis
 tails_data_mapped_true <- tails_data_mapped[tails_data_mapped$CTGAC_R5 > 0, ]
 tails_data_mapped_true$ref_name = tails_data_mapped_true$ref_name_R5  #use ref_name_R5 as ref_name
 # remove heterogenous tails from analysis
 tails_data_mapped_true_no_hetero = tails_data_mapped_true[-grep("hetero", tails_data_mapped_true$tail_type),
     ]
-# remove other type tails from the analysis
+# remove other type tails (for which we can suspect they are not tails but rather origin from improper mapping/repeatmasker) from the analysis
 tails_data_mapped_true_no_hetero_no_other = tails_data_mapped_true_no_hetero[-grep("other",
     tails_data_mapped_true_no_hetero$tail_type), ]
 
@@ -76,7 +79,7 @@ tails_data_mapped_true_no_hetero_no_other[tails_data_mapped_true_no_hetero_no_ot
 tails_data_mapped_true_no_hetero_no_other[tails_data_mapped_true_no_hetero_no_other$tail_type ==
     "UA", ]$tail_type <- "other_no_tail"
 tails_data_mapped_true_no_hetero_no_other <- tails_data_mapped_true_no_hetero_no_other[-grep("other",
-    tails_data_mapped_true_no_hetero_no_other$tail_type), ]
+    tails_data_mapped_true_no_hetero_no_other$tail_type), ] #remove all other from analysis
 
 
 # create classes for A-tail lengths (0,1,2-5,6-10,11-20,21-30,30+)
@@ -122,8 +125,6 @@ tails_data_mapped_true_no_hetero_no_other_tails$tail_type <- as.character(tails_
 tails_data_mapped_true_no_hetero_no_other_tails$tail_type <- factor(tails_data_mapped_true_no_hetero_no_other_tails$tail_type,
     levels = c("U_only", "AU", "no_tail", "A_only"))
 
-levels(as.factor(tails_data_mapped_true_no_hetero_no_other_tails$tail_type))
-
 # create dataframe with PA1 cell_line data
 tails_data_mapped_true_no_hetero_no_other_PA1 <- tails_data_mapped_true_no_hetero_no_other_tails[tails_data_mapped_true_no_hetero_no_other_tails$cell_line ==
     "PA1", ]
@@ -150,11 +151,19 @@ tails_data_mapped_true_no_hetero_no_other_tails_NT <- tails_data_mapped_true_no_
 
 
 tails_data_mapped_true_no_hetero_no_other_tails_NT1 <- tails_data_mapped_true_no_hetero_no_other_tails[(tails_data_mapped_true_no_hetero_no_other_tails$condition ==
-    "NT" & tails_data_mapped_true_no_hetero_no_other_tails$localization == "TOTAL") |
-    grepl("CYTO_NT", tails_data_mapped_true_no_hetero_no_other_tails$sample_name),
+    "NT" & tails_data_mapped_true_no_hetero_no_other_tails$localization %in% c("TOTAL","JADRO")),
     ]
 # filter out tails longer than 64nt - due to a limit of spike-ins used
 tails_data_mapped_true_no_hetero_no_other_tails_NT1 <- tails_data_mapped_true_no_hetero_no_other_tails_NT1[tails_data_mapped_true_no_hetero_no_other_tails_NT1$tail_length <=
+    64, ]
+
+
+tails_data_for_analysis_reporter_overexp <- tails_data_mapped_true_no_hetero_no_other_tails[tails_data_mapped_true_no_hetero_no_other_tails$transcript=="REPORTERL1_overexp",]
+tails_data_for_analysis_reporter_overexp <- tails_data_for_analysis_reporter_overexp[tails_data_for_analysis_reporter_overexp$tail_length <=
+    64, ]
+
+tails_data_for_analysis_reporter_KD <- tails_data_mapped_true_no_hetero_no_other_tails[tails_data_mapped_true_no_hetero_no_other_tails$transcript=="REPORTERL1",]
+tails_data_for_analysis_reporter_KD <- tails_data_for_analysis_reporter_KD[tails_data_for_analysis_reporter_KD$tail_length <=
     64, ]
 
 
@@ -172,48 +181,95 @@ summarize_tails_by_tail_type <- function(tail_data2) {
 
 
 
-pdf("GENOMIC_LINE_NT_tail_types_distribution_cell_line.pdf")
-summary_tail_lengths_table_name <- paste("nt_summarized_tails_lengths_by_type")
-assign(summary_tail_lengths_table_name, summarize_tails_by_tail_type(tails_data_mapped_true_no_hetero_no_other_tails_NT[tails_data_mapped_true_no_hetero_no_other_tails_NT$primer_name ==
-    "L1NGS0", ]))
-summary_tail_lengths_table <- eval(as.symbol(summary_tail_lengths_table_name))
-plot_tail_lengths <- ggplot(summary_tail_lengths_table, aes(x = as.factor(cell_line),
+tails_data_mapped_reporter_overexp_for_terminal <- tails_data_mapped[tails_data_mapped$transcript=='REPORTERL1_overexp',]
+tails_data_mapped_reporter_overexp_for_terminal$condition<-as.character(tails_data_mapped_reporter_overexp_for_terminal$condition)
+tails_data_mapped_reporter_overexp_for_terminal$condition<-as.factor(tails_data_mapped_reporter_overexp_for_terminal$condition)
+
+
+tails_data_mapped_gapdh_overexp_for_terminal <- tails_data_mapped[tails_data_mapped$transcript=='GAPDH' & tails_data_mapped$condition %in% c("CNTRL","TUT7WT","MOV10"),]
+tails_data_mapped_gapdh_overexp_for_terminal$condition<-as.character(tails_data_mapped_gapdh_overexp_for_terminal$condition)
+tails_data_mapped_gapdh_overexp_for_terminal$condition<-as.factor(tails_data_mapped_gapdh_overexp_for_terminal$condition)
+
+
+for (cond in levels(tails_data_mapped_reporter_overexp_for_terminal$condition)) {
+  print(cond)
+  terminal_nucleotides_cond_name=paste("terminal_nucleotides_reporter_overexp",cond,sep="_")
+  #temp2=eval(as.symbol(summary_tail_lengths_table_name))
+  assign(terminal_nucleotides_cond_name,tails_data_mapped_reporter_overexp_for_terminal[tails_data_mapped_reporter_overexp_for_terminal$condition==cond,]$terminal_nucleotides)
+}
+
+terminal_nucleotides_reporter_overexp=list(CTRL=terminal_nucleotides_reporter_overexp_CNTRL,TUT7WT=terminal_nucleotides_reporter_overexp_TUT7WT,MOV10=terminal_nucleotides_reporter_overexp_MOV10,TUT4WT=terminal_nucleotides_reporter_overexp_TUT4WT,TUT7MUT=terminal_nucleotides_reporter_overexp_TUT7MUT,TUT4MUT=terminal_nucleotides_reporter_overexp_TUT4MUT)
+
+for (cond in levels(tails_data_mapped_gapdh_overexp_for_terminal$condition)) {
+  print(cond)
+  terminal_nucleotides_cond_name=paste("terminal_nucleotides_gapdh_overexp",cond,sep="_")
+  #temp2=eval(as.symbol(summary_tail_lengths_table_name))
+  assign(terminal_nucleotides_cond_name,tails_data_mapped_gapdh_overexp_for_terminal[tails_data_mapped_gapdh_overexp_for_terminal$condition==cond,]$terminal_nucleotides)
+}
+
+terminal_nucleotides_gapdh_overexp=list(CTRL=terminal_nucleotides_gapdh_overexp_CNTRL,TUT7WT=terminal_nucleotides_gapdh_overexp_TUT7WT,MOV10=terminal_nucleotides_gapdh_overexp_MOV10)
+
+setwd("/home/smaegol/")
+pdf("fig_4a_reporter.pdf")
+print(ggseqlogo(terminal_nucleotides_reporter_overexp,ncol=2,method='prob'))
+dev.off()
+
+pdf("fig_4a_gapdh.pdf")
+print(ggseqlogo(terminal_nucleotides_gapdh_overexp,ncol=2,method='prob'))
+dev.off()
+
+
+pdf("fig_4B.pdf")
+summary_tail_types_table_name <- paste("repo_over_summarized_tails_types_by_condition")
+assign(summary_tail_types_table_name, summarize_tails_by_tail_type(tails_data_for_analysis_reporter_overexp[tails_data_for_analysis_reporter_overexp$condition %in%
+    c("CNTRL","TUT7WT","TUT4WT","MOV10"), ]))
+summary_tail_types_table <- eval(as.symbol(summary_tail_types_table_name))
+summary_tail_types_table$condition <- as.character(summary_tail_types_table$condition)
+summary_tail_types_table$condition <- factor(summary_tail_types_table$condition,levels=c("CNTRL","TUT7WT","TUT4WT","MOV10"))
+plot_tail_lengths <- ggplot(summary_tail_types_table, aes(x = as.factor(condition),
     fill = tail_type, colours = tail_type)) + geom_bar(aes(y = freq), position = position_stack(),
-    stat = "identity") + scale_y_continuous() + xlab("cell_line") + ylab("% of 3' ends") +
-    scale_fill_grey() + ggtitle(paste("LINE1 reads tails distribution by type, L1NGS0, SSDNA spikes, mapped only"))
+    stat = "identity") + scale_y_continuous() + xlab("ondition") + ylab("fraction of transcripts") +
+    scale_fill_grey() + ggtitle(paste("OVEREXPRESSION"))
+print(plot_tail_lengths)
+dev.off()
+
+pdf("fig_4C.pdf")
+summary_tail_types_table_name <- paste("repo_KD_summarized_tails_types_by_condition")
+assign(summary_tail_types_table_name, summarize_tails_by_tail_type(tails_data_for_analysis_reporter_KD[tails_data_for_analysis_reporter_KD$condition %in%
+    c("CNTRLKD","TUT7KD","TUT4KD","TUT4TUT7KD"), ]))
+summary_tail_types_table <- eval(as.symbol(summary_tail_types_table_name))
+summary_tail_types_table$condition <- as.character(summary_tail_types_table$condition)
+summary_tail_types_table$condition <- factor(summary_tail_types_table$condition,levels=c("CNTRLKD","TUT7KD","TUT4KD","TUT4TUT7KD"))
+plot_tail_lengths <- ggplot(summary_tail_types_table, aes(x = as.factor(condition),
+    fill = tail_type, colours = tail_type)) + geom_bar(aes(y = freq), position = position_stack(),
+    stat = "identity") + scale_y_continuous() + xlab("ondition") + ylab("fraction of transcripts") +
+    scale_fill_grey() + ggtitle(paste("DEPLETION"))
 print(plot_tail_lengths)
 dev.off()
 
 
-#prepare data for seqlogo
-tails_data_mapped_PA1<-tails_data_mapped[tails_data_mapped$cell_line=='PA1',]
-tails_data_mapped_PA1$condition<-as.character(tails_data_mapped_PA1$condition)
-tails_data_mapped_PA1$condition<-as.factor(tails_data_mapped_PA1$condition)
-
-tails_data_mapped_293<-tails_data_mapped[tails_data_mapped$cell_line=='293T',]
-tails_data_mapped_293$condition<-as.character(tails_data_mapped_293$condition)
-tails_data_mapped_293$condition<-as.factor(tails_data_mapped_293$condition)
-
-tails_data_mapped_PA1_ENDOL1<-tails_data_mapped_PA1[tails_data_mapped_PA1$primer_name=='L1NGS0',]
-tails_data_mapped_PA1_ENDOL1$condition<-as.character(tails_data_mapped_PA1_ENDOL1$condition)
-tails_data_mapped_PA1_ENDOL1$condition<-as.factor(tails_data_mapped_PA1_ENDOL1$condition)
-
-for (cond in levels(tails_data_mapped_PA1_ENDOL1$condition)) {
-  print(cond)
-  terminal_nucleotides_cond_name=paste("terminal_nucleotides",cond,sep="_")
-  #temp2=eval(as.symbol(summary_tail_lengths_table_name))
-  assign(terminal_nucleotides_cond_name,tails_data_mapped_PA1_ENDOL1[tails_data_mapped_PA1_ENDOL1$condition==cond,]$terminal_nucleotides)
-}
-
-terminal_nucleotides_knockdown=list(si_ctrl=terminal_nucleotides_CNTRLSIRNA,si_TUT=terminal_nucleotides_TUTSIRNA,si_MOV10C=terminal_nucleotides_MOV10SIRNAC)
-
-pdf("seqlogo_ENDOL1_PA1_L1NGS0_knockdown_window7.pdf")
-print(ggseqlogo(terminal_nucleotides_knockdown,ncol=2,method='prob'))
-print(ggseqlogo(terminal_nucleotides_knockdown,ncol=2,method='bits'))
+pdf("fig_4D.pdf")
+summary_tail_types_table_name <- paste("genomic_L1_summarized_tails_types_by_cell_lines")
+assign(summary_tail_types_table_name, summarize_tails_by_tail_type(tails_data_mapped_true_no_hetero_no_other_tails_NT1[tails_data_mapped_true_no_hetero_no_other_tails_NT1$transcript=="ENDOL1" & tails_data_mapped_true_no_hetero_no_other_tails_NT1$cell_line %in% c("293T","H9","HELAHA","NPC","PA1","MYSZ"), ]))
+summary_tail_types_table <- eval(as.symbol(summary_tail_types_table_name))
+summary_tail_types_table$cell_line <- as.character(summary_tail_types_table$cell_line)
+summary_tail_types_table[summary_tail_types_table$cell_line=='MYSZ',]$cell_line="MOUSE_TESTIS"
+summary_tail_types_table$cell_line <- factor(summary_tail_types_table$cell_line,levels=c("293T","HELAHA","NPC","PA1","H9","MOUSE_TESTIS"))
+plot_tail_lengths <- ggplot(summary_tail_types_table, aes(x = as.factor(cell_line),
+    fill = tail_type, colours = tail_type)) + geom_bar(aes(y = freq), position = position_stack(),
+    stat = "identity") + scale_y_continuous() + xlab("cell_line") + ylab("fraction of transcripts") +
+    scale_fill_grey() + ggtitle(paste("DEPLETION"))
+print(plot_tail_lengths)
+dev.off()pdf("fig_4F.pdf")
+summary_tail_types_table_name <- paste("genomic_L1_summarized_tails_types_by_condition_KD")
+assign(summary_tail_types_table_name, summarize_tails_by_tail_type(tails_data_mapped_true_no_hetero_no_other_PA1_tails_KD[tails_data_mapped_true_no_hetero_no_other_PA1_tails_KD$condition %in%
+    c("CNTRLKD","MOV10KD","TUT4TUT7KD") & tails_data_mapped_true_no_hetero_no_other_PA1_tails_KD$transcript=="ENDOL1", ]))
+summary_tail_types_table <- eval(as.symbol(summary_tail_types_table_name))
+summary_tail_types_table$condition <- as.character(summary_tail_types_table$condition)
+summary_tail_types_table$condition <- factor(summary_tail_types_table$condition,levels=c("CNTRLKD","TUT4TUT7KD","MOV10KD"))
+plot_tail_lengths <- ggplot(summary_tail_types_table, aes(x = as.factor(condition),
+    fill = tail_type, colours = tail_type)) + geom_bar(aes(y = freq), position = position_stack(),
+    stat = "identity") + scale_y_continuous() + xlab("condition") + ylab("fraction of transcripts") +
+    scale_fill_grey() + ggtitle(paste("DEPLETION (PA-1)"))
+print(plot_tail_lengths)
 dev.off()
-
-#head(terminal_nucleotides_NT)
-
-print(ggseqlogo(terminal_nucleotides_NT,ncol=2,method='bits'))
-
-ls()
